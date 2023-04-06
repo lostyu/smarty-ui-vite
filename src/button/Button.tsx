d8ea40a5ad7d8ad21379c4d510c8cd749889df7c
tonysoul <tonysoul@foxmail.com>
2023-04-06 10:39:27 +0800

    test husky

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,6 +1,8 @@
 import { defineComponent, PropType } from "vue";
 import "uno.css";
 
+let a = 123;
+console.log(555);
 export type ISize = "small" | "medium" | "large";
 export type IColor =
   | "black"
@@ -41,6 +43,8 @@ export default defineComponent({
   props,
   setup(props, { slots }) {
     // TODO py-1.5 有小数的情况
+    const a = 123;
+    // a = 123;
     const size = {
       small: {
         x: 2,
